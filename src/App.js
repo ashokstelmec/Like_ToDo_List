51fda9a2aa75946c8c47afc8eecd0e1aaff8faac
Ashok Sah <ashok.stelmec@gmail.com>
2022-07-22 23:01:19 +0530

    ToDo List Look Alike

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const myArray = [
+    { id: "0", name: "Sujal", age: "27" },
+    { id: "1", name: "Aniket", age: "17" },
+    { id: "2", name: "Ashok", age: "26" },
+  ];
+
+  const [code, setCode] = useState(myArray);
+
+  const handleClick = (e) => {
+    setCode([]);
+  };
+
+  const handleChange = (id) => {
+    // alert(id);
+    const myNewArray = code.filter((element) => {
+      return element.id !== id;
+    })
+
+    setCode(myNewArray);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {code.map((item) => {
+        return (
+          <h1 key={item.id}>
+            Name: {item.name} & Age: {item.age}
+            <button className="btn" onClick={ () => handleChange(item.id)}>
+              Remove
+            </button>
+          </h1>
+        );
+      })}
+      <button onClick={handleClick}> Click me</button>
     </div>
   );
 }
